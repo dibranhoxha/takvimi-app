157eef2880420fcbc86c567b05bb36605ec631b0
Dibran Hoxha <dibran.dev@gmail.com>
2022-02-01 11:08:33 +0100

    Fixes Qibla orientation screen // Tried api fetching

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, Alert, Dimensions } from "react-native";
 import Header from "../components/Header";
 import InfoCard from "../components/InfoCard";
 
 const Home = () => {
+    const [times, setTimes] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getTimings();
+        return () => setTimes(null);
+    }, []);
+
+    const getTimings = () => {
+        fetch(
+            "http://api.aladhan.com/v1/calendarByCity?city=Pristina&country=Kosovo&method=4&month=01&year=2022"
+        )
+            .then((response) => response.json())
+            .then((result) =>
+                result.data.map((day) => console.log(day))
+            )
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    };
+
     const list = [
-        {id: 1, title: 'Sabahu', time: '06:00 AM', active: false},
-        {id: 2, title: 'Lindja e Diellit', time: '06:45 AM', active: false},
-        {id: 3, title: 'Dreka', time: '11:30 AM', active: false},
-        {id: 4, title: 'Ikindia', time: '13:54 AM', active: false},
-        {id: 5, title: 'Akshami', time: '16:12 AM', active: false},
-        {id: 6, title: 'Jacia', time: '17:49 AM', active: false},
-    ]
+        { id: 1, title: "Sabahu", time: "06:00 AM", active: false },
+        { id: 2, title: "Lindja e Diellit", time: "06:45 AM", active: false },
+        { id: 3, title: "Dreka", time: "11:30 AM", active: false },
+        { id: 4, title: "Ikindia", time: "13:54 AM", active: false },
+        { id: 5, title: "Akshami", time: "16:12 AM", active: false },
+        { id: 6, title: "Jacia", time: "17:49 AM", active: false },
+    ];
     return (
-        <View style={{padding: 0, margin: 0}}>
+        <View style={{ padding: 0, margin: 0 }}>
             <Header />
             <InfoCard remainingTime={"1 hour 34 minutes left"} list={list} />
         </View>
--- src/screens/QiblaScreen.js
+++ src/screens/QiblaScreen.js
@@ -1,12 +1,138 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from "react";
+import {
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    Dimensions,
+    Image,
+    ImageBackground
+} from "react-native";
+import { Magnetometer } from "expo-sensors";
+import { Grid, Col, Row } from "react-native-easy-grid";
+
+const { height, width } = Dimensions.get("window");
 
 const QiblaScreen = () => {
-    return (
-        <View>
-            <Text>Orientation screen</Text>
-        </View>
-    )
-}
+    const [subscription, setSubscription] = useState(null);
+    const [magnetometer, setMagnetometer] = useState(0);
 
-export default QiblaScreen
+    useEffect(() => {
+        _toggle();
+        return () => {
+            _unsubscribe();
+        };
+    }, []);
+
+    const _toggle = () => {
+        if (subscription) {
+            _unsubscribe();
+        } else {
+            _subscribe();
+        }
+    };
+
+    const _subscribe = () => {
+        setSubscription(
+            Magnetometer.addListener((data) => {
+                setMagnetometer(_angle(data));
+            })
+        );
+    };
+
+    const _unsubscribe = () => {
+        subscription && subscription.remove();
+        setSubscription(null);
+    };
+
+    const _angle = (magnetometer) => {
+        let angle = 0;
+        if (magnetometer) {
+            let { x, y, z } = magnetometer;
+            if (Math.atan2(y, x) >= 0) {
+                angle = Math.atan2(y, x) * (180 / Math.PI);
+            } else {
+                angle = (Math.atan2(y, x) + 2 * Math.PI) * (180 / Math.PI);
+            }
+        }
+        return Math.round(angle);
+    };
+
+    const _direction = (degree) => {
+        if (degree >= 22.5 && degree < 67.5) {
+            return "NE";
+        } else if (degree >= 67.5 && degree < 112.5) {
+            return "E";
+        } else if (degree >= 112.5 && degree < 157.5) {
+            return "SE";
+        } else if (degree >= 157.5 && degree < 202.5) {
+            return "S";
+        } else if (degree >= 202.5 && degree < 247.5) {
+            return "SW";
+        } else if (degree >= 247.5 && degree < 292.5) {
+            return "W";
+        } else if (degree >= 292.5 && degree < 337.5) {
+            return "NW";
+        } else {
+            return "N";
+        }
+    };
+
+    const _degree = (magnetometer) => {
+        return magnetometer - 90 >= 0 ? magnetometer - 90 : magnetometer + 271;
+    };
+
+    return (
+        <Grid
+            style={{
+                backgroundColor:
+                    _degree(magnetometer) >= 137 && _degree(magnetometer) <= 150
+                        ? "green"
+                        : "#fff",
+            }}
+        >
+            <Row style={{ alignItems: "center" }} size={0.1}>
+                <Text
+                    style={{
+                        color: "#000",
+                        fontSize: height / 27,
+                        width: width,
+                        position: "absolute",
+                        textAlign: "left",
+                    }}
+                >
+                    {_degree(magnetometer)}°
+                </Text>
+                <Text style={{
+                        color: "#000",
+                        fontSize: height / 27,
+                        width: width,
+                        position: "absolute",
+                        textAlign: "center",
+                    }}>
+                {360 - magnetometer - 125}
+
+                </Text>
+            </Row>
+
+            <Row style={{ alignItems: "center" }} size={2}>
+                <Col style={{ alignItems: "center" }}>
+                    <Image
+                        source={require("../assets/images/compass.png")}
+                        style={{
+                            height: width - 80,
+                            justifyContent: "center",
+                            alignItems: "center",
+                            resizeMode: "contain",
+                            transform: [
+                                { rotate: 360 - magnetometer - 125 + "deg" },
+                            ],
+                        }}
+                    />
+                </Col>
+            </Row>
+        </Grid>
+    );
+};
+
+export default QiblaScreen;
